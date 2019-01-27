624895a6311f3929f68f8acfc48c74b7c4dbb7f6
Benjamin du Preez <benjamindupreez@gmail.com>
2019-01-27 00:46:56 -0500

    added weather card

--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
 import React, {Component, } from 'react';
 import {Platform, StyleSheet, Text, View, StatusBar} from 'react-native';
-import {Header} from "react-native-elements";
+import IonIcon from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const instructions = Platform.select({
     ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -53,11 +54,41 @@ export default class weather extends Component<Props> {
                         />
                     </View>
                     <View style={styles.header}>
-
                         <Text style={styles.text}> Weather </Text>
                     </View >
                     <View style={{flex:9/10}}>
-                        <Text style={styles.welcome}>{this.state.weather["temperature"]}</Text>
+                        <View style={styles.card}>
+                            <View style={{height: 70, borderBottomWidth: 1, borderBottomColor:'#FAF3F4',textAlign: 'center',
+                                justifyContent: 'center',alignItems:"center", borderBottomLeftRadius:10, borderBottomRightRadius:10,}}>
+                                <Icon
+                                    name={weatherIcons["partly-cloudy-night"]}
+                                    color={'#FAF3F4'}
+                                    size={30}
+                                />
+                                <Text style={styles.cardHeader} onPress={()=> this.props.navigation.navigate('horoscope')}>{this.state.weather["summary"]}</Text>
+                            </View>
+                            <Text style={{margin: 7, color:'#FAF3F4' }}>Temperature: {this.state.weather["temperature"]}°F</Text>
+                            <Text style={{margin: 7, color:'#FAF3F4' }}>Precipitation: {this.state.weather["precipProbability"] * 100}%</Text>
+                            <Text style={{margin: 7, color:'#FAF3F4' }}>Humidity: {this.state.weather["humidity"] * 100}%</Text>
+                            <Text style={{margin: 7, color:'#FAF3F4' }}>Wind: {this.state.weather["windSpeed"]} mph</Text>
+
+                            {/*</View>*/}
+                            {/*<Card*/}
+                            {/*title='Scorpio'>*/}
+                            {/*<View style={{flexDirection: 'row',alignItems: 'space-between', justifyContent: 'center'}}>*/}
+
+                            {/*<Image source={require('./images/scorpio.png')}>*/}
+                            {/*</Image>*/}
+
+
+                            {/*<Text style={{marginBottom: 10}}>*/}
+                            {/*The idea with React Native Elements is more about component structure than actual design.*/}
+                            {/*</Text>*/}
+                            {/*</View>*/}
+                            {/*</Card>*/}
+
+
+                        </View>
                     </View>
                 </View>
             );
@@ -80,6 +111,20 @@ export default class weather extends Component<Props> {
     }
 }
 
+// this maps all the possible icon descriptions provided by Darksky into names for appropriate icons as provided by react-native-vector-icons
+const weatherIcons = {
+    "clear-day" : "weather-sunny",
+    "clear-night" : "weather-sunny",
+    "rain" : "weather-pouring",
+    "snow" : "weather-snowy",
+    "sleet" : "weather-snowy-rainy",
+    "wind" : "weather-windy",
+    "fog" : "weather-fog",
+    "cloudy" : "weather-cloudy",
+    "partly-cloudy-day" : "weather-partlycloudy",
+    "partly-cloudy-night" : "weather-partlycloudy"
+};
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -100,7 +145,7 @@ const styles = StyleSheet.create({
     },
     header: {
         flex:1/10,
-        height: 20,
+        height: 30,
         borderBottomColor: '#6369D1',
         borderBottomWidth:1,
         justifyContent: 'center',
@@ -109,5 +154,19 @@ const styles = StyleSheet.create({
     text:{
         fontSize: 25,
         color:'#6369D1',
-    }
+    },
+    card:{
+        backgroundColor:'#2F405C',
+        height: 222,
+        // marginLeft: 10,
+        // marginRight: 10,
+        margin: 10,
+    },
+    cardHeader:{
+        fontSize: 25,
+        color:'#FAF3F4',
+        textAlign: 'center',
+        justifyContent: 'center',
+
+    },
 });
