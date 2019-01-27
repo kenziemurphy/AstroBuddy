9328b7fccdd3117b09129b02bc7dd2eb014e8e03
McKenzie Murphy <kenziemurphy2828@gmail.com>
2019-01-27 11:29:32 -0500

    horoscope card tweaking

--- horoscopeinfo.js
+++ horoscopeinfo.js
@@ -101,7 +101,7 @@ const styles = StyleSheet.create({
         // marginLeft: 10,
         // marginRight: 10,
         margin: 10,
-        
+
     }
 });
 export default withNavigation(horoscopeinfo)
